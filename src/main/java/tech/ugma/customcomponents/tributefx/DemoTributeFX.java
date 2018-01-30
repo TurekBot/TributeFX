22f51b9a40d3c09246878b98c4a6e9c22cb77590
Bradley Turek <TurekBot@gmail.com>
2018-01-30 14:23:12 -0600

    We're going to have it be a static class that configures any WebView it's given.

--- src/main/java/tech/ugma/customcomponents/tributefx/DemoTributeFX.java
+++ src/main/java/tech/ugma/customcomponents/tributefx/DemoTributeFX.java
@@ -5,12 +5,15 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 public class DemoTributeFX extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        TributeFX textArea = new TributeFX();
+        WebView textArea = new WebView();
+        TributeFX.convertWebView(textArea);
+
         AnchorPane root = new AnchorPane(textArea);
         root.setPadding(new Insets(20));
         root.setBorder(new Border(new BorderStroke(Color.RED, BorderStrokeStyle.DASHED, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
