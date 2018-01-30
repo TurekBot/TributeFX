897e03fae7351433420b54f7ffdc5d396da17914
Bradley Turek <TurekBot@gmail.com>
2018-01-30 14:15:58 -0600

    I don't know where I am, but I'm taking a different direction now.

--- src/main/java/tech/ugma/customcomponents/tributefx/DemoTributeFX.java
+++ src/main/java/tech/ugma/customcomponents/tributefx/DemoTributeFX.java
@@ -1,15 +1,19 @@
 package tech.ugma.customcomponents.tributefx;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class DemoTributeFX extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         TributeFX textArea = new TributeFX();
-        BorderPane root = new BorderPane(textArea);
+        AnchorPane root = new AnchorPane(textArea);
+        root.setPadding(new Insets(20));
+        root.setBorder(new Border(new BorderStroke(Color.RED, BorderStrokeStyle.DASHED, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
         Scene theScene = new Scene(root);
 
 
--- src/main/java/tech/ugma/customcomponents/tributefx/TributeFX.java
+++ src/main/java/tech/ugma/customcomponents/tributefx/TributeFX.java
@@ -1,11 +1,51 @@
 package tech.ugma.customcomponents.tributefx;
 
+import com.sun.javafx.scene.web.skin.HTMLEditorSkin;
+import javafx.application.Platform;
 import javafx.scene.web.HTMLEditor;
 
 public class TributeFX extends HTMLEditor {
 
     public TributeFX() {
         //Set up HTML
+        this.setHtmlText(
+                "<!DOCTYPE html>\n" +
+                        "<html lang=\"en\">\n" +
+                        "<body>\n" +
+                        "    <script>document.write(\"1\");</script>" +
+                        "    <!--Load the files of the @mention library (called Tribute)-->\n" +
+                        "    <link rel=\"stylesheet\" href=\"tribute-js/tribute.css\" />\n" +
+                        "    <script>document.write(\"2\");</script>" +
+                        "    <script src=\"tribute-js/tribute.js\"></script>\n" +
+                        "    <script>document.write(\"3\");</script>" +
+                        "\n" +
+                        // TODO: 1/28/2018 use the body instead of a certain div
+                        "    <!--This is the container that we're going to attach the jtribute to.-->\n" +
+                        "    <div contenteditable=\"true\" class=\"mentionable-container\">I'm alive!</div>\n" +
+                        "    <script>document.write(\"4\");</script>" +
+                        "    <!--And this is the stylesheet that goes along with the container.-->\n" +
+                        "    <link rel=\"stylesheet\" href=\"container.css\" />\n" +
+                        "    <script>document.write(\"5\");</script>" +
+                        "\n" +
+                        "    <!--This script builds the Tribute object and attaches it to the container-->\n" +
+                        "    <script src=\"../../../startTribute.js\"> </script>\n" +
+                        "    <script>document.write(\"6\");</script>" +
+                        "    <script>var href = window.location.href; document.write(href);</script>" +
+
+                        "</body>\n" +
+                        "</html>"
+        );
+
+        HTMLEditorSkin htmlEditorSkin = (HTMLEditorSkin) this.getSkin();
+        Platform.runLater(() -> {
+            if (getScene() == null) {
+                System.out.println("Couldn't apply listener to Scene!");
+            } else {
+                getScene().getWindow().setOnShown(shown -> {
+                    System.out.println("Scene, shown.");
+                });
+            }
+        });
 
 
     }
